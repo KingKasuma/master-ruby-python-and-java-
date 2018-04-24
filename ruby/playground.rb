6aa5abbecfa76f884f0f9f943edf411b2da52741
Mike <lanselot777hailander@gmail.com>
2018-04-24 16:44:00 -0400

    if/else

--- ruby/playground.rb
+++ ruby/playground.rb
@@ -1,6 +1,40 @@
-# What is astring?
-puts "What us your first name?"
-first_name = gets.chomp
-puts "What is your last name?"
-last_name = gets.chomp
-puts "Welcome to the program #{first_name} #{last_name}"
+def multiply(any_number, any_other_number)
+  any_number.to_f * any_other_number.to_f
+end
+
+def divide(any_number, any_other_number)
+  any_number.to_f / any_other_number.to_f
+end
+
+def subtract(any_number, any_other_number)
+  any_number.to_f - any_other_number.to_f
+end
+
+def add(any_number, any_other_number)
+  any_number.to_f + any_other_number.to_f
+end
+
+def mod(any_number, any_other_number)
+  any_number.to_f % any_other_number.to_f
+end
+
+puts "Welcome to the calculator, what would you like to do? 1)multiply, 2) divide, 3) add"
+prompt = gets.chomp
+
+puts "What do you want the first number to be?"
+first_number = gets.chomp
+puts "What do you want the second number to be?"
+second_number = gets.chomp
+
+if prompt == "1"
+ puts "You have chosen to multiply #{first_number} with #{second_number}"
+ puts "The first number multiplied by the second is #{multiply(first_number, second_number)}"
+elsif prompt == "2"
+  puts "You have chosen to divide"
+  puts "The first number divided by the second number is: #{divide(first_number, second_number)}"
+elsif prompt == "3"
+  puts "You have chosen to add"
+  puts "The first number added to the second number is: #{add(first_number, second_number)}"
+else
+  puts "Incorrect choice"
+end
