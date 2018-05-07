e490354d06380534e48fe8e06ce5298d28e77f1b
Mike <lanselot777hailander@gmail.com>
2018-05-07 17:29:33 -0400

    Java first

---
+++ java/Stack.java
@@ -0,0 +1,38 @@
+import java.util.Vector;
+
+public class Stack{
+  private static Vector<Object> stack = new Vector<Object>();
+
+  public static void main (String[] args){
+    System.out.println(empty());
+    push("Hello");
+    System.out.println(empty());
+    push("what");
+    push("hey");
+    push("greetings");
+    push("mashrur");
+
+    System.out.println(pop());
+    System.out.println(pop());
+    System.out.println(peek());
+    System.out.println(peek());
+  }
+
+  public static boolean empty(){
+    return stack.isEmpty();
+  }
+
+  public static Object peek(){
+    return stack.lastElement();
+  }
+
+  public static Object pop(){
+    Object obj = stack.lastElement();
+    stack.remove(obj);
+    return obj;
+  }
+
+  public static void push(Object obj){
+    stack.addElement(obj);
+  }
+}
