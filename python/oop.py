1632052879edfeb0ac95346c18cb9b4fa0583a60
Mike <lanselot777hailander@gmail.com>
2018-05-15 18:22:29 -0400

    POO

--- python/oop.py
+++ python/oop.py
@@ -13,3 +13,11 @@ y.notes("Rock n roll is still king")
 
 x.display()
 y.display()
+
+class Musician(Music):
+    def display(self):
+        print('The notes are = "%s"'%self.data)
+
+mashur = Musician()
+mashur.notes("I don't know how to sing")
+mashur.display()
